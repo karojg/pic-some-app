b9dbc8433f926c1fd170d131394b224568ea7721
Karolina Jimenez <karojg@gmail.com>
2020-04-27 22:36:09 -0600

    Sets Context and fetch data

---
+++ src/context/Context.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const Context = React.createContext();
+
+const ContextProvider = ({children}) => {
+
+  const [allPhotos, setAllPhotos] = useState([])
+
+  const url = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setAllPhotos(data))
+  }, [])
+
+  console.log('allPhotos', allPhotos)
+
+  return (
+        <Context.Provider value={{allPhotos}}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export { ContextProvider, Context }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ContextProvider } from './context/Context'
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <ContextProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ContextProvider>,
   document.getElementById('root')
 );
 
