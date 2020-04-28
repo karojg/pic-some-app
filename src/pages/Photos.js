c5fd401d7216cf07ba822bbad20ea800905124a8
Karolina Jimenez <karojg@gmail.com>
2020-04-27 23:10:27 -0600

    Display photos

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,20 @@
-import React from "react"
+import React, {useContext} from "react"
+import Image from '../components/Image'
+import { getClass } from "../utils"
+
+import { Context }  from '../context/Context'
 
 function Photos() {
+    const { allPhotos } = useContext(Context)
+
+    const data = allPhotos.map((img, index) => (
+      <Image key={img.id} img={img} className={getClass(index)} />
+    ))
+    console.log('data', data)
+
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+          {data}
         </main>
     )
 }
