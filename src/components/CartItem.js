7c5b0d948fc4773afc20667632ac3a67c316277c
Karolina Jimenez <karojg@gmail.com>
2020-05-03 13:23:02 -0600

    Removes unused hooks

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useRef } from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { Context } from '../context/Context'
 import useHover from '../hooks/useHover'
